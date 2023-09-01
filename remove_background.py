b5a6ae9b3bbf584e9002e5aa93bfd9b4d41f17e9
sooraj <soorajks2002@gmail.com>
2023-09-01 22:04:54 +0530

    function will save new image to a location rather than returning it's pxel values

--- remove_background.py
+++ remove_background.py
@@ -1,11 +1,8 @@
 from rembg import remove
 import cv2
 
-def remove_bg(input_path) :
+def remove_bg(input_path, output_path):
 
     input_image = cv2.imread(input_path)
     output_image = remove(input_image)
-    
-    return output_image
-
-print(remove_bg("test.jpg"))
+    cv2.imwrite(output_path, output_image)
