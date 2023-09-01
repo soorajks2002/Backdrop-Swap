3b2b6cab6da0e4bb15efd84667a41602e7a7ffa9
sooraj <soorajks2002@gmail.com>
2023-09-01 22:12:01 +0530

    fixed different image size issue

--- superimpose.py
+++ superimpose.py
@@ -3,24 +3,19 @@ from PIL import Image
 
 def superimpose_img(image_path, background_path, output_path) :
     
-    # input_image = cv2.imread(image_path)
-    # print(input_image.shape)
-    # background_image = cv2.imread(background_path)
-    # print(background_image.shape)
-    
     input_image = Image.open(image_path)
     background_image = Image.open(background_path)
+    
+    input_image = input_image.resize((background_image.width, background_image.height))
     
     x = (background_image.width - input_image.width)//2
     y = (background_image.height - input_image.height)//2
     
+    print(background_image.height, background_image.width)
+    print(input_image.height, input_image.width)
     
-    mask = input_image.convert("L")  # Convert to grayscale
+    mask = input_image.convert("L")
     mask = mask.point(lambda p: p > 0 and 255)
     
     background_image.paste(input_image, (x,y), mask)
     background_image.save(output_path)
-    
-    # output_image = cv2.addWeighted(input_image,1.0,background_image,1.0,0)
-    
-    # cv2.imwrite(output_path,output_image)
